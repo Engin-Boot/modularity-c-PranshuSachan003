e5fdd6f21ac4c22e834ab2e953d6ad23d28b4b1a
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-25 15:33:45 +0530

    Update Part3.cs

--- TelCo.ColorCoder/Part3.cs
+++ TelCo.ColorCoder/Part3.cs
@@ -10,9 +10,9 @@ namespace TelCo.ColorCoder
         {
             // Find the major color in the array and get the index
             int majorIndex = -1;
-            for (int i = 0; i < colorMapMajor.Length; i++)
+            for (int i = 0; i < Program.colorMapMajor.Length; i++)
             {
-                if (colorMapMajor[i] == pair.majorColor)
+                if (Program.colorMapMajor[i] == pair.majorColor)
                 {
                     majorIndex = i;
                     break;
@@ -21,9 +21,9 @@ namespace TelCo.ColorCoder
 
             // Find the minor color in the array and get the index
             int minorIndex = -1;
-            for (int i = 0; i < colorMapMinor.Length; i++)
+            for (int i = 0; i < Program.colorMapMinor.Length; i++)
             {
-                if (colorMapMinor[i] == pair.minorColor)
+                if (Program.colorMapMinor[i] == pair.minorColor)
                 {
                     minorIndex = i;
                     break;
@@ -38,7 +38,7 @@ namespace TelCo.ColorCoder
 
             // Compute pair number and Return  
             // (Note: +1 in compute is because pair number is 1 based, not zero)
-            return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
+            return (majorIndex * Program.colorMapMinor.Length) + (minorIndex + 1);
         }
    }
 }
