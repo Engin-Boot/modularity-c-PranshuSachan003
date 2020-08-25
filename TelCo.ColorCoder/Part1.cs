b3d03a2391de7da385fde9941c889475d4dbb624
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-25 17:16:50 +0530

    Update Part1.cs

--- TelCo.ColorCoder/Part1.cs
+++ TelCo.ColorCoder/Part1.cs
@@ -9,7 +9,10 @@ namespace TelCo.ColorCoder
             internal Color minorColor;
             public override string ToString()
             {
-                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+                string str = "";
+                for(int i=1;i<26;i++)
+                str = str+ string.Format("PairNumber : MajorColor, MinorColor\n", i,Program.colorMapMajor[i],minorColor = Program.colorMapMinor[i]);
+                return str;
             }
         }
 }
