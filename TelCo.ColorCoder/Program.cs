64ae61b4e6df37363846796a6ea9ae7e5f503242
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-09-07 16:47:37 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -16,11 +16,11 @@ namespace TelCo.ColorCoder
         /// <summary>
         /// Array of Major colors
         /// </summary>
-        public static Color[] colorMapMajor;
+        private static Color[] colorMapMajor;
         /// <summary>
         /// Array of minor colors
         /// </summary>
-        public static Color[] colorMapMinor;
+        private static Color[] colorMapMinor;
         /// <summary>
         /// data type defined to hold the two colors of clor pair
         /// </summary>
